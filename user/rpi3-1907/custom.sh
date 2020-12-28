ff2eda5f40580c1957e2199ab131b2322eaca211
5high <5high.net@gmail.com>
2020-12-28 13:39:16 +0800

    Update custom.sh

--- user/rpi3-1907/custom.sh
+++ user/rpi3-1907/custom.sh
@@ -2,3 +2,7 @@
 
 echo "Apply custom.sh"
 
+# add pi 3 BT driver
+mkdir -p files/lib/firmware/brcm
+wget https://sumju.net/BCM4345C0.hcd -o files/lib/firmware/brcm/
+chmod +x files/lib/firmware/brcm/*

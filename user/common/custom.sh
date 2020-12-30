d7bfbd24c76e43a53fab536776ae56ccd50102bc
Marco <marco@MarcodeiMac.local>
2020-12-30 09:13:51 +0800

    rollback files

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -5,20 +5,20 @@
 sed -i 's/192.168.1.1/192.168.1.254/g' package/base-files/files/bin/config_generate
 
 #fix upnpd
-#cp ../user/common/files/upnp package/feeds/luci/luci-app-upnp/root/etc/config/
+cp ../user/common/files/upnp package/feeds/luci/luci-app-upnp/root/etc/config/
 
 #enable KMS service
-#cp ../user/common/files/vlmcsd package/lean/luci-app-vlmcsd/root/etc/config/
+cp ../user/common/files/vlmcsd package/lean/luci-app-vlmcsd/root/etc/config/
 
 #enable bbr
-#cp ../user/common/files/flowoffload package/lean/luci-app-flowoffload/root/etc/config/
+cp ../user/common/files/flowoffload package/lean/luci-app-flowoffload/root/etc/config/
 
 #enable unblock music
-#cp ../user/common/files/unblockmusic package/lean/luci-app-unblockmusic/root/etc/config/
+cp ../user/common/files/unblockmusic package/lean/luci-app-unblockmusic/root/etc/config/
 
 #enable igmproxy
-#cp ../user/common/files/igmpproxy /package/network/services/igmpproxy/
-#cp ../user/common/files/firewall.config package/network/config/firewall/files/
+cp ../user/common/files/igmpproxy /package/network/services/igmpproxy/
+cp ../user/common/files/firewall.config package/network/config/firewall/files/
 
 # Add luci-app-ssr-plus
 pushd package/lean
